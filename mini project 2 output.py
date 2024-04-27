649296a6a72e471cbb35cdcc38367e1015b55f49
NathashaFAlmeda <167543010+NathashaFAlmeda@users.noreply.github.com>
2024-04-27 22:13:16 +0530

    Add files via upload

---
+++ mini project 2 output.py
@@ -0,0 +1,49 @@
+1.Create Account
+2.Login
+3.Logout
+4.Deposit Amount
+5.Withdraw Amount
+6.View Account Details
+7.Exit
+Enter  your choice :1
+enter the account number :123
+enter acconut holder name :nathasha
+Account is successfully created for nathasha. Account Number :123
+1.Create Account
+2.Login
+3.Logout
+4.Deposit Amount
+5.Withdraw Amount
+6.View Account Details
+7.Exit
+Enter  your choice :2
+enter the account number :123
+ Logged into the account 123
+1.Create Account
+2.Login
+3.Logout
+4.Deposit Amount
+5.Withdraw Amount
+6.View Account Details
+7.Exit
+Enter  your choice :6
+Account NUmber :123
+Acount Holder :nathasha
+Account Balance :0
+1.Create Account
+2.Login
+3.Logout
+4.Deposit Amount
+5.Withdraw Amount
+6.View Account Details
+7.Exit
+Enter  your choice :7
+exiting
+1.Create Account
+2.Login
+3.Logout
+4.Deposit Amount
+5.Withdraw Amount
+6.View Account Details
+7.Exit
+Enter  your choice :
